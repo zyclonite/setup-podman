7d93b4e5e3591c0bf6ed248f316fe675d36f22d3
Lukas Prettenthaler <lukas.prettenthaler@tipico.us>
2022-06-21 15:17:29 +0200

    empty test

--- index.test.js
+++ index.test.js
@@ -1,16 +1,3 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
+test('empty test', async () => {
+  //nothing
 });
